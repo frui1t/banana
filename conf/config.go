6c90e41943c34fa034633a160cb7c79bbdb58032
frui1t <gjyjayxs@126.com>
2022-09-20 22:38:18 +0800

    web

--- conf/config.go
+++ conf/config.go
@@ -2,7 +2,9 @@ package conf
 
 import (
 	"banana/model"
+	"banana/server"
 	"fmt"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"gopkg.in/ini.v1"
@@ -14,10 +16,10 @@ type Config struct {
 }
 
 type ServiceConf struct {
-	AppMode      string `ini:"AppMode"`
-	HttpPort     string `ini:"HttpPort"`
-	ReadTimeout  string `ini:"ReadTimeout"`
-	WriteTimeout string `ini:"WriteTimeout"`
+	AppMode      string        `ini:"AppMode"`
+	HttpPort     string        `ini:"HttpPort"`
+	ReadTimeout  time.Duration `ini:"ReadTimeout"`
+	WriteTimeout time.Duration `ini:"WriteTimeout"`
 }
 type MysqlConf struct {
 	DB           string `ini:"DB"`
@@ -39,4 +41,20 @@ func Init() {
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", cfg.MysqlConf.DbUser, cfg.MysqlConf.DbPassword, cfg.MysqlConf.DbHost, cfg.MysqlConf.DbPort, cfg.MysqlConf.DbName)
 	model.Database(dsn, cfg.MysqlConf.MaxIdleConns, cfg.MaxOpenConns)
+
+	r := server.NewRouter()
+
+	r.Run(":8000")
+
+	/*
+		s := &http.Server{
+			Addr:           ":8000",
+			Handler:        r,
+			ReadTimeout:    10,
+			WriteTimeout:   10,
+			MaxHeaderBytes: 1 << 20,
+		}
+		s.ListenAndServe()
+	*/
+
 }
